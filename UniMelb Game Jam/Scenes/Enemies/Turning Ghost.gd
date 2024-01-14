4e7283f6b4e21cef724f046ff1c9260d78f65470
Jun <junyynyyn@users.noreply.github.com>
2024-01-14 14:42:31 +0800

    Added AI for Turning Ghost

---
+++ UniMelb Game Jam/Scenes/Enemies/Turning Ghost.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+var run_direction : Vector2 = Vector2(0,0)
+@export var MAX_SPEED : float = 150.0
+@export var ROAM_SPEED : float = 50.0
+var speed : float = 20.0
+var roam_target : Vector2 = Vector2(0,0)
+
+enum state {ROAMING, RUNNING}
+
+var ghost_state : state = state.ROAMING
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	if ghost_state == state.ROAMING:
+		var direction = (position - roam_target).normalized()
+		var speed = lerp(speed, ROAM_SPEED, 0.5)
+		velocity = direction * speed
+	elif ghost_state == state.RUNNING:
+		velocity.x = run_direction.x * MAX_SPEED
+		velocity.y = run_direction.y * MAX_SPEED
+	move_and_slide()
+
+func _on_detection_area_body_entered(body):
+	ghost_state = state.RUNNING
+	# Get player direction in relation to ghost, normalize and rotate by 90 degrees
+	if (body.is_in_group("Player")):
+		run_direction = -(Global.player.global_position - position).normalized()
+		run_direction = run_direction.rotated(PI/2)
+
+func _on_detection_area_body_exited(body):
+	$RunningTimer.start()
+
+func _on_running_timer_timeout():
+	ghost_state = state.ROAMING
+	velocity = Vector2(0,0)
+
+func _on_roam_timer_timeout():
+	speed = ROAM_SPEED
+	var rng = RandomNumberGenerator.new()
+	roam_target.x = position.x + rng.randf_range(-10.0, 10.0)
+	roam_target.y = position.y + rng.randf_range(-10.0, 10.0)
--- UniMelb Game Jam/Scripts/Player.gd
+++ UniMelb Game Jam/Scripts/Player.gd
@@ -7,7 +7,7 @@ var ghosts_collected = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Global.player = self
 	
 func _input(_event):
 	pass
