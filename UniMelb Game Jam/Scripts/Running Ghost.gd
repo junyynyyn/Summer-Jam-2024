48cb802444f08fd63b205333426a10ecb27533a2
Lucashazmoney <149584093+Lucashazmoney@users.noreply.github.com>
2024-01-14 19:45:56 +1300

    Add files via upload

--- UniMelb Game Jam/Scripts/Running Ghost.gd
+++ UniMelb Game Jam/Scripts/Running Ghost.gd
@@ -2,14 +2,13 @@ extends CharacterBody2D
 
 var health = 200
 var damage = 5
-@export var speed = 200
+@export var speed = 3000
 
 var health_bar: TextureProgressBar
 var is_dead = false
 #var home_pos = Vector2.ZERO
 var target_node = null
-@export var nav_agent: NavigationAgent2D
-
+@onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
 
 #func _enter_tree():
 	#home_pos = self.global_position
@@ -23,10 +22,14 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	if nav_agent.is_navigation_finished():
 		return
-	
+	if target_node:
+		var direction = (self.global_position - target_node.global_position).normalized()
+		var distance = 50
+		var new_target_position = self.global_position + direction * distance
+		nav_agent.target_position = new_target_position
 	var axis = to_local(nav_agent.get_next_path_position()).normalized()
 	var intended_velocity = axis * speed * delta
-	nav_agent.set_velocity(intended_velocity)
+	velocity = intended_velocity
 	move_and_slide()
 
 func recalc_path():
@@ -44,7 +47,7 @@ func _on_path_recalc_timeout():
 func _on_aggro_range_area_entered(area):
 	target_node = area.owner
 	var direction = (self.global_position - target_node.global_position).normalized()
-	var distance = 100
+	var distance = 50  # Adjust this value to control how far the enemy runs
 	var new_target_position = self.global_position + direction * distance
 	nav_agent.target_position = new_target_position
 	print("target acquired")
@@ -56,9 +59,8 @@ func _on_aggro_range_area_exited(area):
 		print("target left")
 
 
-
-
 func _on_navigation_agent_2d_velocity_computed(safe_velocity):
 	velocity = safe_velocity
 	move_and_slide()
 	print("computed")
+
