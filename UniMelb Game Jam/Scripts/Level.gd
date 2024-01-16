897f23efa4fd64c70f54087dcc3ba56dafb639c0
Jun <junyynyyn@users.noreply.github.com>
2024-01-16 14:31:21 +0800

    Fixed Ghost Progress bar not updating correctly

--- UniMelb Game Jam/Scripts/Level.gd
+++ UniMelb Game Jam/Scripts/Level.gd
@@ -10,14 +10,18 @@ var timer_active : bool = true
 var goal : bool = false
 
 func _ready():
+	#Get total ghost count and send to UI
 	ghost_total = $Ghosts.get_child_count()
+	$UI.set_ghost_count_max(ghost_total)
 
 func _process(delta):
+	# Get current ghost count from player and send to UI, also update timer 
 	$UI.update_ghost_count(Global.player.ghosts_collected.size())
 	if (timer_active):
 		timer += delta
 	$UI.update_timer(timer)
 	
+	# Check if player has hit the ghost collected to finish the level
 	if (Global.player.ghosts_collected.size() >= ghost_quota):
 		complete_level()
 	
@@ -29,7 +33,8 @@ func pause_timer():
 	
 func start_timer():
 	timer_active = true
-	
+
+# Show end screen UI and pause game functionaltiy besides UI
 func complete_level():
 	$UI.display_finish(timer)
 	$"UI/Timer Grid".visible = false
--- UniMelb Game Jam/Scripts/ui.gd
+++ UniMelb Game Jam/Scripts/ui.gd
@@ -1,15 +1,17 @@
 extends CanvasLayer
 
-@export var level_select_scene : PackedScene
+var level_select_screen = "res://Scenes/Menus/Level Select.tscn"
 
 signal next_scene
 
 func _ready():
 	update_ghost_count(0)
 
+func set_ghost_count_max(count: int):
+	%GhostCounter.max_value = count
+	
 func update_ghost_count(count: int):
-	%GhostCounter.value += count
-	# Doesnt work, needs to increase by one for each ghost collected and update when they're released
+	%GhostCounter.value = count
 
 func update_timer(time: float):
 	%Timer.text = ("%.2f" % time)
@@ -18,7 +20,6 @@ func display_finish(time: float):
 	%FinishScreen.visible = true
 	#%FinishScreen/TimeDisplay.text = "Final Time: %.2f seconds" % time
 	%FinishScreen/TimeDisplay.text = "%.2f seconds" % time
-	print("Finished Level!")
 
 func _on_replay_button_pressed():
 	get_tree().paused = false
@@ -32,4 +33,4 @@ func _on_next_level_button_pressed():
 
 func _on_level_select_button_pressed():
 	get_tree().paused = false
-	get_tree().change_scene_to_packed(level_select_scene)
+	get_tree().change_scene_to_file(level_select_screen)
