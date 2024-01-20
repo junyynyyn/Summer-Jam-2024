457d59854f96805a4ae8c7c331f1fc93f416d2c4
Lucashazmoney <ciarandseymour@gmail.com>
2024-01-20 15:23:54 +1300

    couple tweaks

--- UniMelb Game Jam/Scripts/Credits.gd
+++ UniMelb Game Jam/Scripts/Credits.gd
@@ -1,14 +1,10 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_ESCAPE:
+			SceneTransition.change_scene("res://Scenes/Menus/main_menu.tscn")
 
 
 func _on_texture_button_pressed():
